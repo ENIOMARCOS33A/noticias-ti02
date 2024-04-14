2435a91dfbe6e17f24a1b20c660bbfdfce7f7fc7
Enio Marcos <eniomlima@hotmail.com>
2024-04-14 00:38:55 -0300

    Criação da navegação desktop, remoção da classe header-mobile e início da responsividade

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const botaoAbrir = document.querySelector("header > button");
 const botaoFechar = document.querySelector("header nav button");
 
@@ -16,23 +15,4 @@ function abrirMenu() {
 function fecharMenu () {
     body.classList.remove("escurecer");
     nav.classList.remove ("abrir");
-=======
-const botaoAbrir = document.querySelector(".header-mobile > button")
-const botaoFechar = document.querySelector("header-mobile nav button")
-
-const body = document.querySelector("body")
-const nav = document.querySelector("header-mobile nav")
-
-botaoAbrir.addEventListener('click', abrirMenu)
-botaoFechar.addEventListener('click', fecharMenu)
-
-function abrirMenu() {
-    body.classList.add("escurecer")
-    nav.classList.add("abrir")
-}
-
-function fecharMenu () {
-    body classList.remove("escurecer")
-    nav classList.remove ("abrir")
->>>>>>> c6c6a5457080d67e1029378c1f68a6bb61abc405
 }
